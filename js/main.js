f9eaed6f9756616c3a105538cf0986c6391511f5
roxberry <mark@roxberries.com>
2013-01-14 14:09:35 -0500

    added panels

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+$(document).ready(function() {
 
+ $('#filterspanel').hide();
+ $('#actionspanel').hide();
+
+// // shows the slickbox on clicking the noted link  
+//  $('#slick-show').click(function() {
+//    $('#slickbox').show('slow');
+//    return false;
+//  });
+// // hides the slickbox on clicking the noted link  
+//  $('#slick-hide').click(function() {
+//    $('#slickbox').hide('fast');
+//    return false;
+//  });
+
+ // toggles the slickbox on clicking the noted link  
+  $('#filters').click(function() {
+    $('#filterspanel').toggle(400);
+    return false;
+  });
+
+   // toggles the slickbox on clicking the noted link  
+  $('#actions').click(function() {
+    $('#actionspanel').toggle(400);
+    return false;
+  });
+
+});
